fedcac6fa929c2c9d8c93523a02e73e383ced998
AleksandraRuminska <alexrumi2000@gmail.com>
2024-10-22 12:10:39 +0200

    Set up base enemy system
    Abstract enemy base class
    Standard Enemy class
    Enemy State interface
    Idle and Attack states' blueprints
    State Controller
    Basic AI agent

---
+++ Assets/Scripts/Enemies/StandardEnemy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandardEnemy : EnemyBaseClass
+{
+
+    public override void Attack(){
+        Debug.Log("Attacking");
+    }
+    public override void Hide(){
+    }
+    public override void Die(){
+        // play dying animation
+        Destroy(this.gameObject);
+    }
+
+}
