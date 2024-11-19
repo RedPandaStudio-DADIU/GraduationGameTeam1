60fd50736b4db268db7a5a54011439a84c8d578d
AleksandraRuminska <alexrumi2000@gmail.com>
2024-11-20 00:22:42 +0100

    Add Xaga materials + special attack vfx

--- Assets/Scripts/Enemies/Boss.cs
+++ Assets/Scripts/Enemies/Boss.cs
@@ -1,29 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 using cowsins;
 
 public class Boss : DiplomatEnemy
 {
     [SerializeField] private List<Transform> weakSpots = new List<Transform>();
+    [SerializeField] private List<Material> materials = new List<Material>();
     [SerializeField] private float explosionRange = 200f;
     [SerializeField] private float damageEpxlosion = 20f;
+    [SerializeField] private GameObject attackPreparationEffect;
     [SerializeField] private GameObject explosionEffect;
     [SerializeField] private GameObject weakSpotDefeatedEffect;
 
     [SerializeField] private AK.Wwise.Event bossExplosionSoundEvent;
     [SerializeField] private AK.Wwise.Event bossChargeSoundEvent;
 
+    // [SerializeField] private AK.Wwise.Event dialogueXaga;
+
+    // [SerializeField] private AK.Wwise.State lostArm;
+    // [SerializeField] private AK.Wwise.State lostLeg;
+    // [SerializeField] private AK.Wwise.State lostStomach;
+
+    // [SerializeField] private AK.Wwise.State nearDeath;
+    // [SerializeField] private AK.Wwise.State Dead;
 
     private Queue<Transform> queueWeakSpots;
-    private bool areWeakSpotsDefeated = false;
+    private Queue<Material> queueMaterials;
 
+    // private Queue<AK.Wwise.State> xagaStates = new Queue<AK.Wwise.State>();
+
+    private bool areWeakSpotsDefeated = false;
+    private Renderer renderer;
+    private GameObject instantiatedExplosion;
 
     void Awake()
     {
         this.SetHealth(200f);
         this.SetStoppingDistance(8f);
         queueWeakSpots = new Queue<Transform>(weakSpots);
+        queueMaterials = new Queue<Material>(materials);
+        // xagaStates.Enqueue(lostArm);
+        // xagaStates.Enqueue(lostLeg);
+        // xagaStates.Enqueue(lostStomach);
+        // dialogueXaga.Post(gameObject);
+        renderer = transform.Find("Xaga").GetComponent<Renderer>();
 
     }
 
@@ -52,6 +74,10 @@ public class Boss : DiplomatEnemy
     public void RemoveFromQueue()
     {
         Transform weakspot = queueWeakSpots.Dequeue();
+        // AK.Wwise.State state = xagaStates.Dequeue();
+        // state.SetValue();
+        Material material = queueMaterials.Dequeue();
+        this.renderer.material = material;
         GameObject weakspotDefeatedVFX = Instantiate(weakSpotDefeatedEffect, weakspot.position, Quaternion.identity);
 
     }
@@ -103,6 +129,27 @@ public class Boss : DiplomatEnemy
     public override AK.Wwise.Event GetChargeSound()
     {
         return this.bossChargeSoundEvent;
+    }
+
+    public GameObject GetAttackPrepVFX()
+    {
+        return this.attackPreparationEffect;
+    }
+
+    public void InstantiateAttackPrepVFX()
+    {
+        // instantiatedExplosion = Instantiate(attackPreparationEffect, transform.position, Quaternion.identity);
+        attackPreparationEffect.SetActive(true);
+        attackPreparationEffect.GetComponent<VisualEffect>().Play();
+    }
+
+    public void StopAttackPrepVFX()
+    {
+        // instantiatedExplosion.GetComponent<VisualEffect>().Stop();
+        // Destroy(instantiatedExplosion);
+        attackPreparationEffect.SetActive(false);
+        attackPreparationEffect.GetComponent<VisualEffect>().Stop();
+
     }
 
 }
