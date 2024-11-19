c5f57ca6f1c6afb88f625c4b69d82cfc9388b5a7
AleksandraRuminska <alexrumi2000@gmail.com>
2024-11-19 21:19:36 +0100

    Add music

--- Assets/Scripts/environment/MusicManager.cs
+++ Assets/Scripts/environment/MusicManager.cs
@@ -6,11 +6,19 @@ using AK.Wwise;
 public class MusicManager : MonoBehaviour
 {
     [SerializeField] private AK.Wwise.Event musicEvent;
+    [SerializeField] private AK.Wwise.State combatState;
+    [SerializeField] private AK.Wwise.State nonCombatState;
+    [SerializeField] private AK.Wwise.State levelState;
+    [SerializeField] private AK.Wwise.State intensityLevelLowState;
+    [SerializeField] private AK.Wwise.State intensityLevelHighState;
+    [SerializeField] private AK.Wwise.State intensityNoneState;
+    private int currentLevel = 0;
+
     void Start()
     {
-        AkSoundEngine.SetSwitch("MainMusicSW", "Lv1_hangard", gameObject);
-        AkSoundEngine.SetSwitch("Lv1_Combat", "Lv1_combat_intensity", gameObject);
-        AkSoundEngine.SetSwitch("Lv1_combat_intensity", "Lv1_combat_low", gameObject);
+        nonCombatState.SetValue();
+        intensityNoneState.SetValue();
+        levelState.SetValue();
 
         musicEvent.Post(gameObject);
 
@@ -19,5 +27,37 @@ public class MusicManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void CheckIfSameState(int level){
+        if (currentLevel == level) return;
+        else {
+            SetStateOfCombat(level);
+        }
+    }
+
+    public void SetStateOfCombat(int level){
+        currentLevel = level;
+        switch(level){
+            case 1:
+                combatState.SetValue();
+                intensityNoneState.SetValue();
+                // intensityLevelLowState.SetValue();
+                Debug.Log("Playing music level 1 - low combat");
+                break;
+            case 2:
+                combatState.SetValue();
+                intensityNoneState.SetValue();
+                Debug.Log("Playing music level 2 - high combat");
+                // intensityLevelHighState.SetValue();
+                break;
+            default:
+                nonCombatState.SetValue();
+                intensityNoneState.SetValue();
+                Debug.Log("Playing music level 0 - no combat");
+
+                break;
+
+        }
     }
 }
